059aa2721f94d35a95a5de9a23582d13d48e394a
AJBalter <antbalter@gmail.com>
2023-10-22 20:33:49 +0300

    hw pw expiration check update

--- src/main/java/telran/java48/security/UserDetailsServiceImpl.java
+++ src/main/java/telran/java48/security/UserDetailsServiceImpl.java
@@ -1,5 +1,9 @@
 package telran.java48.security;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -21,11 +25,27 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		UserAccount userAccount = userAccountRepository.findById(username)
 				.orElseThrow(() -> new UsernameNotFoundException(username));
-		String[] roles = userAccount.getRoles()
+		
+		
+		List<String> rolesList = userAccount.getRoles()
 										.stream()
 										.map(r -> "ROLE_" + r.toUpperCase())
-										.toArray(String[]::new);
+										.collect(Collectors.toList());
+		
+		if(isPasswordExpired(userAccount.getPasswordExpiryDate())) {
+			rolesList.add("ROLE_EXPIRED");			
+		}
+		
+		String[] roles = rolesList.stream().toArray(String[]::new);
+		
 		return new User(username, userAccount.getPassword(), AuthorityUtils.createAuthorityList(roles));
 	}
+	
+	public boolean isPasswordExpired(LocalDate expDate) {
+        if (expDate == null || expDate.isAfter(LocalDate.now())) {
+                return false;
+            }
+        return true;
+    }
 
 }
